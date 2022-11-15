9dd521915c2b44bc705c100fdab4af9e5b5d213a
xlzxq <104225234+xlzxq@users.noreply.github.com>
2022-11-15 19:58:52 +0000

    Update yuri.js

--- commands/nsfw/yuri.js
+++ commands/nsfw/yuri.js
@@ -3,5 +3,6 @@ module.exports = ({
     aliases: ["lesbian"],
     code: `
     $api[https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=yuri&json=1;$random[1;99];file_url]
+    $onlyNSFW[Please send this command in a NSFW channel!]
     `
 })
