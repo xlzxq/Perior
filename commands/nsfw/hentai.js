1db69de0580742028a898e02b33347eb6418f3db
xlzxq <104225234+xlzxq@users.noreply.github.com>
2022-11-15 19:58:42 +0000

    Update hentai.js

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -2,5 +2,6 @@ module.exports = ({
     name: "hentai",
     code: `
     $api[https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=1boy1girl+score:>=500+-video+-yuri+-yaoi+-gay&json=1;$random[1;99];file_url]
+    $onlyNSFW[Please send this command in a NSFW channel!]
     `
 })
