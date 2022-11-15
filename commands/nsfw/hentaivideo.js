4eb28e4181324115fb1341f0a90e1de8556dc18e
xlzxq <104225234+xlzxq@users.noreply.github.com>
2022-11-15 19:58:45 +0000

    Update hentaivideo.js

--- commands/nsfw/hentaivideo.js
+++ commands/nsfw/hentaivideo.js
@@ -3,5 +3,6 @@ module.exports = ({
     aliases: ["hvideo"],
     code: `
     $api[https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=1boy1girl+score:>=500+video&json=1;$random[1;99];file_url]
+    $onlyNSFW[Please send this command in a NSFW channel!]
     `
 })
