4e36935db1dfcefd6ea5a12c3a5bd571c942f04b
xlzxq <104225234+xlzxq@users.noreply.github.com>
2022-11-15 19:58:49 +0000

    Update yaoi.js

--- commands/nsfw/yaoi.js
+++ commands/nsfw/yaoi.js
@@ -3,5 +3,6 @@ module.exports = ({
     aliases: ["gay"],
     code: `
     $api[https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=yaoi&json=1;$random[1;99];file_url]
+    $onlyNSFW[Please send this command in a NSFW channel!]
     `
 })
